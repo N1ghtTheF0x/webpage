9168988ce09e319d45aff3868bad2a85eaec8164
N1ghtTheF0x <60095145+N1ghtTheF0x@users.noreply.github.com>
2021-03-09 12:31:52 +0100

    Webpack Update
    - now using Webpack

---
+++ bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.info("Hello World");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lY2xpcHNlLWVuZ2luZS13ZWIvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxPQUFPLENBQUMsSUFBSSxDQUFDLGFBQWEsQ0FBQyIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmluZm8oXCJIZWxsbyBXb3JsZFwiKSJdLCJzb3VyY2VSb290IjoiIn0=
